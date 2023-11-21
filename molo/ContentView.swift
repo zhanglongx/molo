97729b717e40a936d2a0a7a5a4f07a7ed9d20176
Longxiao Zhang iMac-Pro <zhanglongx@gmail.com>
2023-11-21 19:02:09 +0800

    Initial Commit

---
+++ molo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  molo
+//
+//  Created by zhlx on 2023/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ molo/moloApp.swift
@@ -0,0 +1,17 @@
+//
+//  moloApp.swift
+//  molo
+//
+//  Created by zhlx on 2023/11/21.
+//
+
+import SwiftUI
+
+@main
+struct moloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
