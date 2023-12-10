819dcfb1568717f08200d767f9300ef96157fe55
Longxiao Zhang MacBook-Air <zhanglongx@gmail.com>
2023-12-10 22:11:26 +0800

    Refactor loading YAML file from iCloud container

--- molo/Model/UserCost.swift
+++ molo/Model/UserCost.swift
@@ -5,27 +5,23 @@ let filePath = "Documents/molo.yml"
 
 func load() -> [Stock]? {
 
-    let iCloudURL: URL?
-
     let fileManager = FileManager.default
 
     if let iCloudURL = fileManager.url(forUbiquityContainerIdentifier: nil)?
                                 .appendingPathComponent(filePath) {
-        iCloudURL = iCloudURL
-    } else {
-        return nil
-    }
-
-    do {
-        let yamlString = try String(contentsOf: iCloudURL, encoding: .utf8)
-        let yamlObject = try Yams.load(yaml: yamlString)
-        let decoder = JSONDecoder()
-        if let yamlData = try? JSONSerialization.data(withJSONObject: yamlObject, options: []),
-            let costs = try? decoder.decode([Stock].self, from: yamlData) {
-            return costs
+        do {
+            let yamlString = try String(contentsOf: iCloudURL, encoding: .utf8)
+            let yamlObject = try Yams.load(yaml: yamlString)
+            let decoder = JSONDecoder()
+            if let yamlData = try? JSONSerialization.data(withJSONObject: yamlObject, options: []),
+                let costs = try? decoder.decode([Stock].self, from: yamlData) {
+                    return costs
+            }
+        } catch {
+            print("Failed to load or parse YAML: \(error)")
         }
-    } catch {
-        print("Failed to load or parse YAML: \(error)")
+    } else {
+        print("Failed to create URL to iCloud container.")
     }
 
     return nil
