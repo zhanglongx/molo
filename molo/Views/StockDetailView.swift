20d77299a1197d6d534991dfa129a98e1df1b94e
Longxiao Zhang iMac-Pro <zhanglongx@gmail.com>
2023-12-20 10:06:11 +0800

    Add StockFormView.swift

--- molo/Views/StockDetailView.swift
+++ molo/Views/StockDetailView.swift
@@ -1,6 +1,5 @@
 
 import SwiftUI
-import Combine
 
 struct StockDetailView: View {
     @Environment(StockModel.self) var model: StockModel
@@ -9,54 +8,51 @@ struct StockDetailView: View {
 
     @Binding var stock: Stock
 
-    @State private var inputCost: String = ""
-
     var body: some View {
-        Form {
-            Section(header: Text("基本信息")) {
-                TextField("名称", text: $stock.name)
-                TextField("代码", text: $stock.symbol)
-            }
+        NavigationStack {
+            StockFormView(stock: $stock)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("编辑")
+                    .font(.title)
+                    .bold()
+                }
 
-            Section(header: Text("成本")) {
-                TextField("成本", text: $inputCost)
-                    .onAppear(
-                        perform: {
-                            inputCost = String(format: "%.2f", stock.cost ?? 0)
-                        }
-                    )
-                    .onReceive(Just(inputCost)) { newValue in
-                        let filtered = newValue.filter { "0123456789.".contains($0) }
-                        if filtered != newValue {
-                            inputCost = filtered
-                            stock.cost = Double(inputCost)
-                        }
+                ToolbarItem(placement: cancelButtonPlacement) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("取消")
                     }
-            }
+                }
 
-            Button(action: {
-                model.add(stock)
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("保存")
+                ToolbarItem(placement: saveButtonPlacement) {
+                    Button {
+                        model.update(stock)
+                    } label: {
+                        Text("完成")
+                    }
+                }
             }
-
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("取消")
-                .foregroundStyle(.red)
-            }
+            #if os(macOS)
+            .padding()
+            #endif
         }
     }
-}
-
-struct StockDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        @State var stock = Stock(symbol: "SH601231", name: "环旭电子", cost: 0) 
-
-        StockDetailView(stock: $stock)
-            .environment(StockModel())
+    
+    private var cancelButtonPlacement: ToolbarItemPlacement {
+        #if os(macOS)
+        .cancellationAction
+        #else
+        .navigationBarLeading
+        #endif
+    }
+    
+    private var saveButtonPlacement: ToolbarItemPlacement {
+        #if os(macOS)
+        .confirmationAction
+        #else
+        .navigationBarTrailing
+        #endif
     }
 }
-            
---
+++ molo/Views/StockFormView.swift
@@ -0,0 +1,46 @@
+
+import SwiftUI
+import Combine
+
+struct StockFormView: View {
+    @Environment(StockModel.self) var model: StockModel
+
+    @Binding var stock: Stock
+
+    @State private var inputCost: String = "0.00"
+
+    var body: some View {
+        Form {
+            Section(header: Text("基本信息")) {
+                TextField("名称", text: $stock.name)
+                TextField("代码", text: $stock.symbol)
+            }
+
+            Section(header: Text("成本")) {
+                TextField("成本", text: $inputCost)
+                    .onAppear(
+                        perform: {
+                            inputCost = String(format: "%.2f", stock.cost ?? 0)
+                        }
+                    )
+                    .onReceive(Just(inputCost)) { newValue in
+                        if let cost = Double(newValue) {
+                            stock.cost = cost
+                        } else {
+                            inputCost = ""
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct StockDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        @State var stock = Stock(symbol: "SH601231", name: "环旭电子", cost: 0.00) 
+
+        StockDetailView(stock: $stock)
+            .environment(StockModel())
+    }
+}
+            
